76ff27faf2b9b6994e2f044ba95e1619822a62dd
MRISOFTWARE\Rinkal.Tailor <Rinkal.Tailor@Mrisoftware.com>
2023-07-21 14:02:56 +0530

    Implemented listview

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'Fruit.dart';
 
 void main() => runApp(const MaterialApp(
       home: Home(),
@@ -15,19 +17,95 @@ class Home extends StatelessWidget {
             title: Text('my first app'),
             centerTitle: true,
             backgroundColor: Colors.red[600]),
-        body: Center(
-            child: Column(children: <Widget>[
-          Text(
-            'hello, ninjas!',
-            style: TextStyle(
-              fontSize: 25.0,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2.0,
-              color: Colors.grey[600],
-              fontFamily: 'IndieFlower',
-            ),
-          ),
-          /*Image(
+        body: SingleChildScrollView(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+              SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(children: <Widget>[
+                    Container(
+                        color: Colors.cyanAccent,
+                        //EdgeInsets.all apply same for all side, symmetric-vertical, horizontal, fromLTRB all need to be defined
+                        padding: const EdgeInsets.all(2.0),
+                        margin: EdgeInsets.all(5.0),
+                        child: TextButton(
+                          onPressed: () {
+                            print("You clicked text button");
+                          },
+                          child: Text("I am flat button"),
+                          style: ButtonStyle(
+                              textStyle: MaterialStateProperty.all(TextStyle(
+                                fontSize: 20.0,
+                              )),
+                              backgroundColor:
+                                  MaterialStateProperty.all(Colors.red[600]),
+                              foregroundColor:
+                                  MaterialStateProperty.all(Colors.black)),
+                        )),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Center(
+                          child: ElevatedButton(
+                              onPressed: () {
+                                print("I am elevated button");
+                              },
+                              child: Text("Elevated button"),
+                              style: ButtonStyle(
+                                  elevation: MaterialStateProperty.all(20.0),
+                                  textStyle: MaterialStateProperty.all(
+                                      TextStyle(fontSize: 20.0)),
+                                  backgroundColor: MaterialStateProperty.all(
+                                      Color.fromARGB(25, 256, 25, 25))))),
+                    ),
+                    Center(
+                        child: ElevatedButton.icon(
+                            onPressed: () {
+                              print("Elevated with icon button");
+                            },
+                            icon: Icon(Icons.ac_unit),
+                            label: Text("Elevated with icon")))
+                  ])),
+              Text(
+                'hello, Rinkal is here!',
+                style: TextStyle(
+                  fontSize: 25.0,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.0,
+                  color: Colors.grey[600],
+                  fontFamily: 'IndieFlower',
+                ),
+              ),
+              IconButton(
+                onPressed: () {
+                  print('you clicked icon button');
+                },
+                icon: Center(child: Icon(Icons.ac_unit)),
+                color: Colors.amber,
+              ),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    flex: 3,
+                    child: Container(
+                      color: Colors.cyanAccent,
+                      padding: EdgeInsets.all(50.0),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 4,
+                    child: Container(
+                      color: Colors.amberAccent,
+                      padding: EdgeInsets.all(50.0),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10.0,
+                  )
+                ],
+              ),
+              /*Image(
             */ /* This was from web
             image: NetworkImage(
 
@@ -35,14 +113,109 @@ class Home extends StatelessWidget {
           ),*/ /*
             image: AssetImage('assets/netflix_logo.png'),
           )*/
-          Image.asset('assets/netflix_logo.png'),
-          Image.network(
-              'https://images.ctfassets.net/kq1hxg0iw5cx/SMmVLMchPRCsBZXMwePFa/3efe7e2e3fbd2733eff1dc4ba7fc5165/banner__1_.png')
-        ])),
+              Image.asset('assets/netflix_logo.png'),
+              Image.network(
+                  'https://images.ctfassets.net/kq1hxg0iw5cx/SMmVLMchPRCsBZXMwePFa/3efe7e2e3fbd2733eff1dc4ba7fc5165/banner__1_.png')
+            ])),
         floatingActionButton: FloatingActionButton(
           backgroundColor: Colors.red[600],
-          onPressed: () {},
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const secondPage()),
+            );
+          },
           child: Text('click'),
+        ),
+      ),
+    );
+  }
+}
+
+class secondPage extends StatefulWidget {
+  const secondPage({Key? key}) : super(key: key);
+
+  @override
+  State<secondPage> createState() => _secondPageState();
+}
+
+class _secondPageState extends State<secondPage> {
+  @override
+  List<Fruit> fruitName = [
+    Fruit(fruitName: "Apple", fruitColor: "Red"),
+    Fruit(fruitName: "Oranges", fruitColor: "Orange"),
+    Fruit(fruitName: "Cherries", fruitColor: "Red"),
+    Fruit(fruitName: "Cranberries", fruitColor: "Red"),
+    Fruit(fruitName: "Grapes", fruitColor: "Purpule"),
+    Fruit(fruitName: "Pears", fruitColor: "Red"),
+    Fruit(fruitName: "Pomegranates", fruitColor: "Red"),
+    Fruit(fruitName: "Raspberries", fruitColor: "Pink"),
+    Fruit(fruitName: "Strawberries", fruitColor: "Red"),
+  ];
+
+  Widget fruitTemplate(fruit) {
+    return FruitCard(fruit: fruit);
+  }
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text("Listview"),
+      ),
+      body: ListView(
+        /*
+          children: fruitName
+              .map(
+                  (e) => Text('Fruit: ${e.fruitName} has Color: ${e.fruitColor}'))
+              .toList(),*/
+        children: fruitName.map((fruit) => fruitTemplate(fruit)).toList(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text(
+          ">",
+          style: TextStyle(
+            fontSize: 30.0,
+          ),
+        ),
+      ),
+    ));
+  }
+}
+
+class FruitCard extends StatelessWidget {
+  final Fruit fruit;
+
+  const FruitCard({
+    required this.fruit,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.yellowAccent,
+      margin: EdgeInsets.all(5.0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Text(
+                fruit.fruitName,
+                style: TextStyle(fontSize: 20.0, color: Colors.black),
+              ),
+              SizedBox(
+                width: 10.0,
+                height: 10.0,
+              ),
+              Text(
+                fruit.fruitColor,
+                style: TextStyle(fontSize: 15.0, color: Colors.blue),
+              )
+            ],
+          ),
         ),
       ),
     );
