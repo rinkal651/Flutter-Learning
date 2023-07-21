6bdb1aaec475b1712ad08b369bf6fca76187ea65
MRISOFTWARE\Rinkal.Tailor <Rinkal.Tailor@Mrisoftware.com>
2023-07-21 16:12:28 +0530

    made calculator

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'Fruit.dart';
 
-void main() => runApp(const MaterialApp(
+void main() =>
+    runApp(const MaterialApp(
       home: Home(),
     ));
 
@@ -22,90 +24,93 @@ class Home extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
-              SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(children: <Widget>[
-                    Container(
-                        color: Colors.cyanAccent,
-                        //EdgeInsets.all apply same for all side, symmetric-vertical, horizontal, fromLTRB all need to be defined
-                        padding: const EdgeInsets.all(2.0),
-                        margin: EdgeInsets.all(5.0),
-                        child: TextButton(
-                          onPressed: () {
-                            print("You clicked text button");
-                          },
-                          child: Text("I am flat button"),
-                          style: ButtonStyle(
-                              textStyle: MaterialStateProperty.all(TextStyle(
-                                fontSize: 20.0,
-                              )),
-                              backgroundColor:
-                                  MaterialStateProperty.all(Colors.red[600]),
-                              foregroundColor:
-                                  MaterialStateProperty.all(Colors.black)),
-                        )),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Center(
-                          child: ElevatedButton(
+                  SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(children: <Widget>[
+                        Container(
+                            color: Colors.cyanAccent,
+                            //EdgeInsets.all apply same for all side, symmetric-vertical, horizontal, fromLTRB all need to be defined
+                            padding: const EdgeInsets.all(2.0),
+                            margin: EdgeInsets.all(5.0),
+                            child: TextButton(
                               onPressed: () {
-                                print("I am elevated button");
+                                print("You clicked text button");
                               },
-                              child: Text("Elevated button"),
+                              child: Text("I am flat button"),
                               style: ButtonStyle(
-                                  elevation: MaterialStateProperty.all(20.0),
                                   textStyle: MaterialStateProperty.all(
-                                      TextStyle(fontSize: 20.0)),
-                                  backgroundColor: MaterialStateProperty.all(
-                                      Color.fromARGB(25, 256, 25, 25))))),
-                    ),
-                    Center(
-                        child: ElevatedButton.icon(
-                            onPressed: () {
-                              print("Elevated with icon button");
-                            },
-                            icon: Icon(Icons.ac_unit),
-                            label: Text("Elevated with icon")))
-                  ])),
-              Text(
-                'hello, Rinkal is here!',
-                style: TextStyle(
-                  fontSize: 25.0,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 2.0,
-                  color: Colors.grey[600],
-                  fontFamily: 'IndieFlower',
-                ),
-              ),
-              IconButton(
-                onPressed: () {
-                  print('you clicked icon button');
-                },
-                icon: Center(child: Icon(Icons.ac_unit)),
-                color: Colors.amber,
-              ),
-              Row(
-                children: <Widget>[
-                  Expanded(
-                    flex: 3,
-                    child: Container(
-                      color: Colors.cyanAccent,
-                      padding: EdgeInsets.all(50.0),
-                    ),
-                  ),
-                  Expanded(
-                    flex: 4,
-                    child: Container(
-                      color: Colors.amberAccent,
-                      padding: EdgeInsets.all(50.0),
+                                      TextStyle(
+                                        fontSize: 20.0,
+                                      )),
+                                  backgroundColor:
+                                  MaterialStateProperty.all(Colors.red[600]),
+                                  foregroundColor:
+                                  MaterialStateProperty.all(Colors.black)),
+                            )),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Center(
+                              child: ElevatedButton(
+                                  onPressed: () {
+                                    print("I am elevated button");
+                                  },
+                                  child: Text("Elevated button"),
+                                  style: ButtonStyle(
+                                      elevation: MaterialStateProperty.all(
+                                          20.0),
+                                      textStyle: MaterialStateProperty.all(
+                                          TextStyle(fontSize: 20.0)),
+                                      backgroundColor: MaterialStateProperty
+                                          .all(
+                                          Color.fromARGB(25, 256, 25, 25))))),
+                        ),
+                        Center(
+                            child: ElevatedButton.icon(
+                                onPressed: () {
+                                  print("Elevated with icon button");
+                                },
+                                icon: Icon(Icons.ac_unit),
+                                label: Text("Elevated with icon")))
+                      ])),
+                  Text(
+                    'hello, Rinkal is here!',
+                    style: TextStyle(
+                      fontSize: 25.0,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 2.0,
+                      color: Colors.grey[600],
+                      fontFamily: 'IndieFlower',
                     ),
                   ),
-                  SizedBox(
-                    width: 10.0,
-                  )
-                ],
-              ),
-              /*Image(
+                  IconButton(
+                    onPressed: () {
+                      print('you clicked icon button');
+                    },
+                    icon: Center(child: Icon(Icons.ac_unit)),
+                    color: Colors.amber,
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        flex: 3,
+                        child: Container(
+                          color: Colors.cyanAccent,
+                          padding: EdgeInsets.all(50.0),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 4,
+                        child: Container(
+                          color: Colors.amberAccent,
+                          padding: EdgeInsets.all(50.0),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      )
+                    ],
+                  ),
+                  /*Image(
             */ /* This was from web
             image: NetworkImage(
 
@@ -113,10 +118,10 @@ class Home extends StatelessWidget {
           ),*/ /*
             image: AssetImage('assets/netflix_logo.png'),
           )*/
-              Image.asset('assets/netflix_logo.png'),
-              Image.network(
-                  'https://images.ctfassets.net/kq1hxg0iw5cx/SMmVLMchPRCsBZXMwePFa/3efe7e2e3fbd2733eff1dc4ba7fc5165/banner__1_.png')
-            ])),
+                  Image.asset('assets/netflix_logo.png'),
+                  Image.network(
+                      'https://images.ctfassets.net/kq1hxg0iw5cx/SMmVLMchPRCsBZXMwePFa/3efe7e2e3fbd2733eff1dc4ba7fc5165/banner__1_.png')
+                ])),
         floatingActionButton: FloatingActionButton(
           backgroundColor: Colors.red[600],
           onPressed: () {
@@ -160,27 +165,32 @@ class _secondPageState extends State<secondPage> {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-      appBar: AppBar(
-        title: Text("Listview"),
-      ),
-      body: ListView(
-        /*
+          appBar: AppBar(
+            title: Text("Listview"),
+          ),
+          body: ListView(
+            /*
           children: fruitName
               .map(
                   (e) => Text('Fruit: ${e.fruitName} has Color: ${e.fruitColor}'))
               .toList(),*/
-        children: fruitName.map((fruit) => fruitTemplate(fruit)).toList(),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Text(
-          ">",
-          style: TextStyle(
-            fontSize: 30.0,
+            children: fruitName.map((fruit) => fruitTemplate(fruit)).toList(),
           ),
-        ),
-      ),
-    ));
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const CalculatorApp()),
+              );
+            },
+            child: Text(
+              ">",
+              style: TextStyle(
+                fontSize: 30.0,
+              ),
+            ),
+          ),
+        ));
   }
 }
 
@@ -219,5 +229,152 @@ class FruitCard extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class CalculatorApp extends StatefulWidget {
+  const CalculatorApp({Key? key}) : super(key: key);
+
+  @override
+  State<CalculatorApp> createState() => _CalculatorAppState();
+}
+
+class _CalculatorAppState extends State<CalculatorApp> {
+  late TextEditingController et1controller = new TextEditingController();
+  late TextEditingController et2controller = new TextEditingController();
+
+  String text1 = "";
+  String text2 = "";
+  String ans = "";
+
+  @override
+  void dispose() {
+    et1controller.dispose();
+    et2controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+        title: Text("My Calculator"),
+    ),
+    body: Center(
+    child: Padding(
+      padding: EdgeInsets.all(20.0),
+      child: Column(
+          children: [ Row(
+          children: [
+            Text("Num1:  ( $text1 )    "),
+            Expanded(
+              child: TextField(
+                controller: et1controller,
+                onChanged: (String value) {
+                  setState(() {
+                    text1 = et1controller.text;
+                  });
+                },
+              ),
+            ), SizedBox(height: 20),
+          ],
+        ),
+      Row(
+        children: [
+          Text("Num2:  ( $text2 )    "),
+          Expanded(
+              child: TextField(
+                controller: et2controller,
+                onChanged: (String value){
+                  setState(() {
+                    text2 = et2controller.text;
+                  });
+                },
+              )
+          )
+        ],
+      ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            TextButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.cyanAccent),
+                  textStyle: MaterialStateProperty.all(TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w700
+                  ))
+                ),
+            onPressed: () {
+              setState(() {
+                ans = (int.parse(text1) + int.parse(text2)).toString();
+              });
+            }, child: Text("+")),
+            TextButton(
+                style:  ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.cyanAccent),
+                    textStyle: MaterialStateProperty.all(TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w700
+                    ))
+                ),
+                onPressed: (){
+              setState(() {
+                ans = (int.parse(text1) - int.parse(text2)).toString();
+              });
+            }, child: Text("-")),
+            TextButton(
+                style:  ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.cyanAccent),
+                    textStyle: MaterialStateProperty.all(TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w700
+                    ))
+                ),
+                onPressed: (){
+              setState(() {
+                ans = (int.parse(text1) * int.parse(text2)).toString();
+              });
+            }, child: Text("*")),
+            TextButton(
+                style:  ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.cyanAccent),
+                    textStyle: MaterialStateProperty.all(TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w700
+                    ))
+                ),
+                onPressed: (){
+              setState(() {
+                ans = (int.parse(text1)/int.parse(text2)).toString();
+              });
+            }, child: Text("/"))
+          ],
+        ),
+        Text("ans = $ans",
+        style: TextStyle(
+          fontSize: 20.0,
+        ),)
+      ]),
+    ),));
+    
+
+    /*      child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    TextField(
+                      controller: controller,
+                      onChanged: (String value) {
+                        setState(() {
+                          text = controller.text;
+                        });
+                      },
+                    ),
+                    SizedBox(height: 20),
+                    Text(text),
+                  ],
+                ))));*/
   }
 }
